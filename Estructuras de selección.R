1c46e9c7a0dd39337758d60e7c69eb9869da6d96
FrancoEscamilla15 <93997753+FrancoEscamilla15@users.noreply.github.com>
2021-11-09 09:52:23 -0600

    Add files via upload

---
+++ Estructuras de selección.R
@@ -0,0 +1,18 @@
+numero <- 6
+umbral <- 36
+
+if(numero^2>=umbral){
+  print("Si es mayor")
+}else{print("No es mayor")}
+
+mafer <- 1.51
+franco <- 1.80
+
+ifelse(mafer<franco,"Si, es un gigante","No, esta enana")
+
+x <- 15 
+x
+if(x<20){print("falso")}
+
+x <- 15
+ifelse(x >25, "esto es falso","esto es verdadero")
